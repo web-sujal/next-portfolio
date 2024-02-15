e4ce257c04480ee9c546c2459c9a5fe6ea05248a
Sujal <web.sujal@gmail.com>
2024-02-15 18:19:36 +0530

    bug fixes in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,10 +17,9 @@ export default function RootLayout({ children }) {
           <div className="h-24">
             <Navbar />
           </div>
-          <div className="h-[calc(100vh - 6rem)]">{children}</div>
+          <div className="h-[calc(100vh-6rem)]">{children}</div>
         </div>
       </body>
     </html>
   );
 }
-
