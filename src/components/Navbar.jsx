195b7e73fadb11beb99933eeaf234dbe75f31ea9
Sujal <web.sujal@gmail.com>
2024-02-15 16:16:04 +0530

    add route and initialized root layout

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      {/* Logo */}
+      <div>
+        <Link
+          href="/"
+          className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
+        >
+          <span className="text-white mx-2">Sujal</span>
+          <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">
+            .dev
+          </span>
+        </Link>
+      </div>
+
+      {/* Responsive Menu */}
+      <div className="">2</div>
+    </div>
+  );
+};
+
+export default Navbar;
