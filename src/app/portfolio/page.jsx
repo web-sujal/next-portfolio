93b47fa734f9d1532060c32e471791cefcfc7d44
Sujal <web.sujal@gmail.com>
2024-02-19 20:16:36 +0530

    completed portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,51 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { useRef } from "react";
+
+const items = [
+  {
+    id: 1,
+    color: "from-red-300 to-blue-300",
+    title: "React Commerce",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 2,
+    color: "from-blue-300 to-violet-300",
+    title: "Next.js Medium Blog",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 3,
+    color: "from-violet-300 to-purple-300",
+    title: "Vanilla Book App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "https://lama.dev",
+  },
+  {
+    id: 4,
+    color: "from-purple-300 to-red-300",
+    title: "Spotify Music App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    link: "https://lama.dev",
+  },
+];
 
 const PortfolioPage = () => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({ target: ref });
+
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-78%"]);
+
   return (
     <motion.div
       className="h-screen"
@@ -10,7 +53,63 @@ const PortfolioPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      Portfolio Page
+      <div className="h-[600vh]" ref={ref}>
+        <div className="flex h-[calc(100vh-6rem)] w-screen items-center justify-center text-center text-8xl">
+          MY WORKS
+        </div>
+
+        <div className="sticky top-0 flex h-screen items-center gap-4 overflow-hidden">
+          <motion.div style={{ x }} className="flex">
+            <div className="flex h-screen w-[67vw] items-center justify-center bg-gradient-to-r from-purple-300 to-red-300" />
+            {items.map((item) => (
+              <div
+                className={`flex h-screen w-screen items-center justify-center bg-gradient-to-r ${item.color}`}
+                key={item.id}
+              >
+                <div className="flex flex-col gap-8 text-white">
+                  <h1>{item.title}</h1>
+                  <div className="relative">
+                    <Image src={item.img} alt={item.title} fill />
+                  </div>
+                  <p>{item.desc}</p>
+                  <Link href={item.link}>
+                    <button>See Demo</button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+      <div className="flex h-screen w-screen flex-col items-center justify-center gap-16 text-center">
+        <h1 className="mt-8 text-8xl">Do you have a project?</h1>
+        <div className="relative">
+          <motion.svg
+            animate={{ rotate: 360 }}
+            transition={{ ease: "linear", duration: 8, repeat: Infinity }}
+            viewBox="0 0 300 300"
+            className="h-64 w-64 md:h-[500px] md:w-[500px]"
+          >
+            <defs>
+              <path
+                id="circlePath"
+                d="M 150, 150 m -60, 0 a 60, 60 0 0, 1 120, 0 a 60, 60 0 0, 1 -120, 0"
+              />
+            </defs>
+            <text fill="#000">
+              <textPath xlinkHref="#circlePath" className="text-xl">
+                Front-end Developer
+              </textPath>
+            </text>
+          </motion.svg>
+          <Link
+            href="/contact"
+            className="absolute inset-0 m-auto flex h-16 w-16 items-center justify-center rounded-full bg-black text-white md:h-28 md:w-28"
+          >
+            Hire Me
+          </Link>
+        </div>
+      </div>
     </motion.div>
   );
 };
