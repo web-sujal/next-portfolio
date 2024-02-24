0fb050b5532affc98cab68750cfbf6b9cb288534
Sujal <web.sujal@gmail.com>
2024-02-24 21:10:06 +0530

    update url of loader.js to netlify hosted url

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 export default function myImageLoader({ src, width, quality }) {
   if (src.startsWith("https://images.pexels.com")) return src;
 
-  return `https://example.com/${src}?w=${width}&q=${quality || 75}`;
+  return `https://web-sujal-portfolio.netlify.app/${src}?w=${width}&q=${quality || 75}`;
 }
